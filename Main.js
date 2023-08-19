41a2258df4ba0e3dc75ca2747a395ff07973cca2
Vadym <vadimnaumnaum@gmail.com>
2023-08-20 00:56:55 +0200

    lessen 3 time 30 m

--- Main.js
+++ Main.js
@@ -1,3 +1,8 @@
+
+
+
+// lesson 3 time 30 m
+
 let name = 'Vadym naum'
 var age = 32
 
@@ -108,3 +113,55 @@ console.log('======= prompt ==========')
 
 
 //============ End =======================
+
+// ============== Work with arr ============
+console.log('work with arr ========')
+
+const arr = ['Anna','Helen','Fox'];
+arr.push('Richard'); // add with last
+arr.push(123);
+arr.push("Jane");
+arr.pop();         // pop() remove last element
+arr.shift();      // sdwig shift all element and remove first element
+arr.unshift("Olga"); // shift all element end add first element
+console.log(arr)
+let arr2 = arr.slice(1,3); // copy array from 1 to 3 = copy two elements index one and two
+console.log(arr2)
+arr.splice(1,2,"some val"); //delete start 1 index and 2 element and add to this place
+console.log(arr);
+let arr3 = arr.concat(['Learn','js']);
+console.log(arr3);
+
+// ======== work with type and for =======
+const word = 'Hello';
+const res = Array.from(word); // make array from word
+console.log(typeof arr);
+console.log(Array.isArray(arr));
+console.log(arr instanceof Array);
+
+// =========  arguments ============
+const funct1 = function () {
+    console.log(arguments);
+    const res = Array.from(arguments);
+    let sum1;
+    res.forEach(i => sum1+= i)
+    console.log(sum1);
+    res.forEach(i=>console.log(i));
+    //res.join(', '); // return from "res" string
+}
+funct1(1,23,4,7);
+
+const arr1 = [21,33,41,64,123,443,2];
+
+arr1.forEach((iten,index)=>{
+    console.log(index + ', ' + iten + ', ' )
+})
+
+// string
+// number
+// boolean
+// undefind
+// null
+// Symbol
+// bigInt
+// object
